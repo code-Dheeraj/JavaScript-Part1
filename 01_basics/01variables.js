5fae744cee4378b0a9118eecafaa8b6c665831e3
code-Dheeraj <72144099+code-Dheeraj@users.noreply.github.com>
2024-01-11 17:49:56 +0000

    variables file done

---
+++ 01_basics/01variables.js
@@ -0,0 +1,27 @@
+// single line comment
+
+/*  
+
+single line comment
+multi line comment
+const - to declare constant
+var - to declare variable but it can be accessed within a function 
+let - to declare variable but it can be access only within a block 
+
+let should be used insted of var
+we can also declare variable without using let and var 
+
+console.log()
+console.table()
+
+*/
+
+
+console.log(fname)
+
+const userId =7758;  // const is used to 
+let Sname ="dheeraj"; //
+var fname ="david"; // Variables declared with var are function-scoped, meaning their scope is limited to the function in which they are declared.
+city = "chandigarh ";
+
+console.table([userId,Sname,fname,city]);
